87205f004cd8c06dbb957e683cd7487f1ddf2855
Relaced422 <Optquenum@gmail.com>
2025-12-03 08:59:41 +0100

    Add header component and update images
    Introduces a reusable header in html/parts/header.php and updates index.php to use it. Adds new image assets for UI elements and updates logo.png. Implements a basic login form in login.php.

--- html/index.php
+++ html/index.php
@@ -5,29 +5,21 @@ require_once __DIR__ . '/connection.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tobrary</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
-<body>
-<header class="bg-[#1F1F1F] flex justify-around items-center p-4 border-2 border-white">
-    <div class="flex flex-col gap-6">
-        <button class="bg-[#7B7B7B] rounded-lg text-center p-4 w-[10vw] border border-white text-white">Cataloog</button>
-        <button class="bg-[#7B7B7B] rounded-lg text-center p-4 w-[10vw] border border-white text-white">Aanbieding</button>
-    </div>
-    <div>
-        <img src="/img/logo.png" alt="Tobrary Logo" class="h-[20vh]">
-    </div>
-    <div class="flex flex-col gap-6">
-        <button class="bg-[#7B7B7B] rounded-lg text-center p-4 w-[10vw] border border-white text-white">Account</button>
-        <button class="bg-[#7B7B7B] rounded-lg text-center p-4 w-[10vw] border border-white text-white">Bestellingen</button>
-    </div>
-</header>
-<main class="bg-[url('img/blackrecycled.jpg')] h-[80vh] bg-cover bg-center">
 
-</main>
-<footer></footer>
+<body>
+    <main class="bg-[url('img/blackrecycled.jpg')] h-[80vh] bg-cover bg-center">
+        <?php include __DIR__ . '/parts/header.php'; ?>
+        
+    </main>
+    <footer></footer>
 </body>
+
 </html>
